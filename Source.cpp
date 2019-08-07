9918102224ac38d5cc41f8f936af344098239ca3
Jin3004 <naktaikyrie@kagi.be>
2019-08-08 06:23:05 +0900

    added a source file.

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Header.h"
+
+void func() {
+  return;
+}
+
+int main() {
+  Connection a(func, 3000);
+  return 0;
+}
