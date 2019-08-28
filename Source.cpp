2019edd21c4ec621b00cea639a81b6ad3bce791b
Jin <53703270+Jin3004@users.noreply.github.com>
2019-08-29 00:40:58 +0900

    不要なので削除

--- Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Header.h"
-int main() {
-  Jin::GameConnect g(3000);
-  const char* data = g.Get();
-  int x = *(int*)data;
-  std::cout << 2 * x << std::endl;
-  return 0;
-}
